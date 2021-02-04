a80462767c3b886c5b4ca08958db0db908873850
Tim <timjohnson@csumb.edu>
2021-02-04 14:04:10 -0800

    Created login and create account button. Created login page

--- app/src/main/java/com/example/project_1_438/MainActivity.java
+++ app/src/main/java/com/example/project_1_438/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.project_1_438;
-//comment
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+        Button createAccountBtn = (Button)findViewById(R.id.createAccountBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /**
+                 * need to still check to make sure the credentials match - Tim
+                 */
+
+                Intent intent = new Intent(getApplicationContext(), loginPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
