4ec19a8fba05a2e112d1cb967039df7e00c9ddeb
Ty <tjharo2@gmail.com>
2021-02-11 13:44:19 -0800

    Creation of Display Page Testing

---
+++ app/src/main/java/com/example/project_1_438/Display_Page.java
@@ -0,0 +1,14 @@
+package com.example.project_1_438;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Display_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display__page);
+    }
+}
