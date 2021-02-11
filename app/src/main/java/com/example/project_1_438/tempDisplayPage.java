d600ee1e44999506d14d48dbda867c773f9e1a21
Tim <timjohnson@csumb.edu>
2021-02-10 19:08:10 -0800

    Finished the Login page...can update looks later. I set up a temporary display page to make sure it changes to the right screen and the right time.

---
+++ app/src/main/java/com/example/project_1_438/tempDisplayPage.java
@@ -0,0 +1,14 @@
+package com.example.project_1_438;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tempDisplayPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_display_page);
+    }
+}
