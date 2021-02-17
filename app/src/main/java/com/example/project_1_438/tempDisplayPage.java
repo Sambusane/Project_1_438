0ec969a067f69d4a811d951d4b648b009494ad24
Ty <tjharo2@gmail.com>
2021-02-16 18:52:53 -0800

    API connection established. Info not yet displayed properly.

--- app/src/main/java/com/example/project_1_438/tempDisplayPage.java
+++ app/src/main/java/com/example/project_1_438/tempDisplayPage.java
@@ -7,9 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.project_1_438.DAO.Database;
 import com.example.project_1_438.DAO.UserDao;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class tempDisplayPage extends AppCompatActivity {
@@ -17,6 +26,7 @@ public class tempDisplayPage extends AppCompatActivity {
     Button deleteAccountButton;
     int userID;
     private UserDao mUserDao;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,7 @@ public class tempDisplayPage extends AppCompatActivity {
         getSupportActionBar().setTitle("Welcome " + userID);
         adminButton = findViewById(R.id.admin_button);
         deleteAccountButton = findViewById(R.id.delete_button);
+        textViewResult = findViewById(R.id.text_view_result);
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,6 +64,37 @@ public class tempDisplayPage extends AppCompatActivity {
             }//if you comment out this whole set on click listener function, then the app runs fine.
             //maybe check the xml to see if button needs something
             //maybe create a new activity, but i dont think so since it will go back to main activity
+        });
+
+        String zipCode = "95076";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post: posts) {
+                    String content ="";
+                    content += "ID: "+post.getId()+"\n";
+                    content += "User ID: "+post.getUserId()+"\n";
+                    content += "Title: "+post.getTitle()+"\n";
+                    content += "Text: "+post.getText()+"\n\n";
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
         });
     }
 }
